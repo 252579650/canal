05328cf4b3ce7054d1ab8eb0d19b5be4bb572cda
zhanglei <lei.zhang@sumscope.com>
2020-04-17 16:10:07 +0800

    init

--- canal-admin/canal-admin-server/src/main/java/com/alibaba/otter/canal/admin/service/impl/ExtracterSinkMapperServiceImpl.java
+++ canal-admin/canal-admin-server/src/main/java/com/alibaba/otter/canal/admin/service/impl/ExtracterSinkMapperServiceImpl.java
@@ -56,37 +56,9 @@ public class ExtracterSinkMapperServiceImpl implements ExtracterSinkMapperServic
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        Properties properties = new Properties();
-        File configFile = new File(".." + File.separator + CONF_DIR + File.separator + "db.properties");
-        if (!configFile.exists()) {
-            URL url = TemplateConfigLoader.class.getClassLoader().getResource("");
-            if (url != null) {
-                configFile = new File(url.getPath() + "db.properties" + File.separator);
-            }
-        }
-        InputStream is = new FileInputStream(configFile);
-        try {
-            properties.load(new InputStreamReader(is, "UTF-8"));
-            Enumeration<Object> keys = properties.keys();
-            while (keys.hasMoreElements()) {
-                String key = (String) keys.nextElement();
-                System.out.println(Arrays.asList(properties.getProperty(key).split(",")));
-            }
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            is.close();
-        }
-    }
-
     public Set<String> querySourceDBNames() {
         return cache.keySet();
     }
-
 
     public List<String> queryTableNames(String key) {
         return cache.get(key);
@@ -324,8 +296,12 @@ public class ExtracterSinkMapperServiceImpl implements ExtracterSinkMapperServic
             List<ETLModelVO.SinkModel> sinkModels = model.getSinkModels();
             if (!CollectionUtils.isEmpty(sinkModels)) {
                 for (ETLModelVO.SinkModel sink : sinkModels) {
+                    ExtracterSink extracterSink = ExtracterSink.find.query().where().eq("ip", sink.getIp()).eq("port", sink.getPort()).eq("sinkOrigin", sink.getSinkOrigin()).findOne();
+                    if (extracterSink != null) {
+                        throw new RuntimeException("该SINK已存在");
+                    }
                     //保存sink信息
-                    ExtracterSink extracterSink = new ExtracterSink();
+                    extracterSink = new ExtracterSink();
                     extracterSink.setIp(sink.getIp());
                     extracterSink.setPort(sink.getPort());
                     extracterSink.setSinkOrigin(sink.getSinkOrigin());
